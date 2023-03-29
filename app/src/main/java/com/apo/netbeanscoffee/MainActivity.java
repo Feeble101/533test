d32666cadc4a1be0243468831dee19f153f91c5a
samelfrink <123603202+samelfrink@users.noreply.github.com>
2023-03-29 12:52:05 -0500

    Initial Commit
    Sample template

---
+++ app/src/main/java/com/apo/netbeanscoffee/MainActivity.java
@@ -0,0 +1,49 @@
+package com.apo.netbeanscoffee;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.apo.netbeanscoffee.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button signUpButton, signInButton, signInAdminBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        signUpButton = (Button) findViewById(R.id.main_sign_up_btn);
+        signInButton = (Button) findViewById(R.id.main_sign_in_btn);
+        signInAdminBtn =(Button) findViewById(R.id.main_admin_sign_in_btn);
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        signInAdminBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,AdminLoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
